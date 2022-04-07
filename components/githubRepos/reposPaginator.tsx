b950b360ab7aa2cdf37e923114d8e00934e1e0b5
JoniRinta-Kahila <joni.rintakahila@opp.eduvantaa.fi>
2022-04-07 20:23:38 +0300

    add repo sorting

--- components/githubRepos/reposPaginator.tsx
+++ components/githubRepos/reposPaginator.tsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react'
 import ReactPaginate from 'react-paginate'
 import { IRepo } from '../../types/githubData'
 import RepoCard from './repoCard'
+import styles from '../../styles/reposPaginator.module.scss'
+import { RepoSortStyle } from '../../types/repoSort'
+import _ from 'lodash'
 
 type ReposPaginatorProps = {
   itemsPerPage: number
@@ -10,9 +13,10 @@ type ReposPaginatorProps = {
 
 type ItemsProps = {
   items: IRepo []
+  sorter: RepoSortStyle
 }
 
-const Items: React.FC<ItemsProps> = ({ items }) => {
+const Items: React.FC<ItemsProps> = ({ items, sorter }) => {
   return (
     <>
       {
@@ -22,7 +26,11 @@ const Items: React.FC<ItemsProps> = ({ items }) => {
   )
 }
 
+const defaultSort = RepoSortStyle.recent;
+
 const ReposPaginator: React.FC<ReposPaginatorProps> = ({ itemsPerPage, items }) => {
+
+  const [sorter, setSorter] = useState<RepoSortStyle>(defaultSort)
 
   const [currentItems, setCurrentItems] = useState<IRepo[]>([]) // TODO fix type!
   const [pageCount, setPageCount] = useState(0)
@@ -31,10 +39,11 @@ const ReposPaginator: React.FC<ReposPaginatorProps> = ({ itemsPerPage, items }) 
   const [itemOffset, setItemOffset] = useState(0)
 
   useEffect(() => {
+    const sortedItems = sortRepos(sorter)(items)
     const endOffset = itemOffset + itemsPerPage
-    setCurrentItems(items.slice(itemOffset, endOffset))
-    setPageCount(Math.ceil(items.length / itemsPerPage))
-  }, [itemOffset, itemsPerPage, items])
+    setCurrentItems(sortedItems.slice(itemOffset, endOffset))
+    setPageCount(Math.ceil(sortedItems.length / itemsPerPage))
+  }, [itemOffset, itemsPerPage, items, sorter])
 
   const handlePageClick = (e: any) => {
     const newOffset = (e.selected * itemsPerPage) % items.length
@@ -43,7 +52,12 @@ const ReposPaginator: React.FC<ReposPaginatorProps> = ({ itemsPerPage, items }) 
   
   return (
     <>
-      <Items items={currentItems} />
+      <div className={styles.sorters}>
+        <input type='radio' value={RepoSortStyle.recent} name='sort' checked={sorter === RepoSortStyle.recent} onChange={() => setSorter(RepoSortStyle.recent)} /> Recent
+        <input type='radio' value={RepoSortStyle.stars} name='sort' checked={sorter === RepoSortStyle.stars} onChange={() => setSorter(RepoSortStyle.stars)} /> Stars
+        <input type='radio' value={RepoSortStyle.language} name='sort' checked={sorter === RepoSortStyle.language} onChange={() => setSorter(RepoSortStyle.language)} /> Language
+      </div>
+      <Items items={currentItems} sorter={sorter} />
       <ReactPaginate
         breakLabel='...'
         nextLabel='next >'
@@ -55,6 +69,19 @@ const ReposPaginator: React.FC<ReposPaginatorProps> = ({ itemsPerPage, items }) 
       />
     </>
   )
+}
+
+function sortRepos(type: RepoSortStyle): (input: IRepo[]) => IRepo[] {  
+  return input => {
+    switch(type){
+      case RepoSortStyle.recent:
+        return _.sortBy(input, x => -new Date(x.updated_at ?? -1).getTime())
+      case RepoSortStyle.stars:
+        return _.sortBy(input, x => -(x.stargazers_count ?? -1))
+      case RepoSortStyle.language:
+        return _.sortBy(input, x => x.language)
+    }
+  }
 }
 
 export default ReposPaginator
