ef12b35aa84cf7fea956cdecbb290a98f8e83255
JoniRinta-Kahila <joni.rintakahila@opp.eduvantaa.fi>
2022-04-27 17:30:55 +0300

    update contactform styles

--- components/contactUs/contactform.tsx
+++ components/contactUs/contactform.tsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import styles from '../../styles/contactform.module.scss'
 import handleSubmit from './submitForm'
+import { BeatLoader } from 'react-spinners'
+import { BsCheck2Circle } from 'react-icons/bs'
 
 const Contactform: React.FC = () => {
   const [loading, setLoading] = useState<boolean>(false)
@@ -14,18 +16,20 @@ const Contactform: React.FC = () => {
   const [ready, setReady] = useState<boolean>(false)
   const [response, setResponse] = useState<any>()
 
+  const buttonRef = useRef<HTMLButtonElement>(null)
+
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     setLoading(true)
 
-    
+
     const submitResponse = await handleSubmit({
       name: name,
       company: company,
       email: email,
       phone: phone,
       message: message,
-      subject: subject,      
+      subject: subject,
     }, e)
 
     setResponse(submitResponse)
@@ -41,9 +45,23 @@ const Contactform: React.FC = () => {
       setReady(true)
       setLoading(false)
 
-      return;
+      return
     }
   }
+
+  useEffect(() => {
+    if (!buttonRef.current) return
+    buttonRef.current.style.background = loading ? 'grey' : 'orangeRed'
+  }, [loading])
+
+  if (ready) return (    
+    <div className={styles.container}>
+      <div className={styles.sendReady}>
+        <BsCheck2Circle size={55} color='lightGreen' />
+        <h2>Thank you!</h2>
+      </div>
+    </div>
+  )
 
   return (
     <div className={styles.container}>
@@ -52,29 +70,68 @@ const Contactform: React.FC = () => {
       </div>
       <form onSubmit={(e) => onSubmit(e)} className={styles.form}>
         <div className={styles.inputs}>
-          <input value={name} onChange={e => setName(e.target.value)} required type="text" placeholder='Name *' disabled={loading} />
-          <input value={company} onChange={e => setCompany(e.target.value)} type="text" placeholder='Company' disabled={loading} />
-          <input value={email} onChange={e => setEmail(e.target.value)} required type="email" placeholder='Email *' disabled={loading} />
-          <input value={phone} onChange={e => setPhone(e.target.value)} type="tel" placeholder='Phone' disabled={loading} />
-          <input value={subject} onChange={e => setSubject(e.target.value)} required type="text" placeholder='Subject *' disabled={loading} />
+          <input
+            value={name}
+            onChange={e => setName(e.target.value)}
+            required type="text"
+            placeholder='Name *'
+            disabled={loading}
+          />
+          <input
+            value={company}
+            onChange={e => setCompany(e.target.value)}
+            type="text"
+            placeholder='Company'
+            disabled={loading}
+          />
+          <input
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            required type="email"
+            placeholder='Email *'
+            disabled={loading}
+          />
+          <input
+            value={phone}
+            onChange={e => setPhone(e.target.value)}
+            type="tel"
+            placeholder='Phone'
+            disabled={loading}
+          />
+          <input
+            value={subject}
+            onChange={e => setSubject(e.target.value)}
+            required type="text"
+            placeholder='Subject *'
+            disabled={loading}
+          />
         </div>
         <div className={styles.message}>
-          <textarea value={message} onChange={e => setMessage(e.target.value)} required placeholder='Message *'></textarea>
-          <button type='submit' className={styles.submit}>Send</button>
+          <textarea value={message}
+            onChange={e => setMessage(e.target.value)}
+            required
+            placeholder='Message *'
+            disabled={loading}
+          />
+          <button ref={buttonRef} type='submit' className={styles.submit}>
+            {
+              loading
+                ? <BeatLoader size={8} />
+                : 'Send'
+            }
+          </button>
         </div>
       </form>
-      <p style={{color:'red'}}>
+      <p style={{ color: 'red' }}>
         {
           response?.valid === false ? 'Err. Form is not valid. Please check fields.' : ''
         }
       </p>
-      <p style={{color:'red'}}>
+      <p style={{ color: 'red' }}>
         {
           response?.success === false ? 'Err. Back end does not respond.' : ''
         }
       </p>
-      {/* <p style={{color:'red'}}>Err. Form is not valid. Please check fields.</p> */}
-      {/* <p style={{color:'red'}}>Err. Back end does not respond.</p> */}
     </div>
   )
 }
