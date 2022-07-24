d01d08731c685eb22c82466068fada416ef7390d
JoniRinta-Kahila <joni.rintakahila@opp.eduvantaa.fi>
2022-07-24 16:52:04 +0300

    update main layout

--- components/layouts/main.tsx
+++ components/layouts/main.tsx
@@ -1,17 +1,8 @@
-import Link from 'next/link'
 import type { LayoutProps } from '../../types/pageWithLayouts'
 import Navbar from '../navbar/navbar'
 import styles from '../../styles/mainLayout.module.scss'
 import Footer from '../footer'
 import ModalContextProvider from '../context/modalContextProvider'
-
-const MenuItems: React.FC = () => (
-  <div className={styles.menuItems}>
-    <Link href='/'>HOME</Link>
-    <Link href='/cv'>CV</Link>
-    <Link href='/projects'>PROJECTS</Link>
-  </div>
-)
 
 const Main: LayoutProps = ({ children }) => {
   return (
