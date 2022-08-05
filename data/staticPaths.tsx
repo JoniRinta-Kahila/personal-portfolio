ac5655e4b2ece79dc7087e8498dfb431db55a717
JoniRinta-Kahila <joni.rintakahila@opp.eduvantaa.fi>
2022-08-05 12:12:01 +0300

    update sitemap

--- data/staticPaths.tsx
+++ data/staticPaths.tsx
@@ -1,17 +1,19 @@
 import * as fs from 'fs'
 
-const baseUrl = process.env.NODE_ENV === 'production' ? './' : 'pages';
+const baseUrl = process.env.NODE_ENV === 'production' ? './pages' : 'pages';
+
+const doNotInclude = [
+  'api',
+  '_app.tsx',
+  '404.tsx',
+  'sitemap.xml.tsx',
+]
 
 export const staticPaths = fs
   .readdirSync(baseUrl)
   .filter(staticPage => {
-    return ![
-      'api',
-      '_app.tsx',
-      '404.tsx',
-      'sitemap.xml.tsx',
-    ].includes(staticPage)
+    return doNotInclude.includes(staticPage) ? null : staticPage.replace('.tsx', '');
   })
   .map(staticPagePath => {
-    return `${baseUrl}/${staticPagePath}`
+    return `${baseUrl}/${staticPagePath.replace('.tsx', '')}`
   })
